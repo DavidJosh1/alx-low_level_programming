e8348e06ce47017e9e74855f3fed1ccdabaa721d
DavidJosh1 <stanleyjssh@gmail.com>
2022-09-12 16:41:31 -0700

    Write a C program that prints exactly and that piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,24 @@
+#include <stdio.h>
+
 #include <unistd.h>
-//
-// main-prints the standrad error sandwiched around the  standard outpu/t
- // Return:returns 1
- 
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 			return (1);
+
 }
 
