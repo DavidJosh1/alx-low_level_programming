51b473141c8ac2154b3df91b0f0c04e2c44afdbd
DavidJosh1 <stanleyjssh@gmail.com>
2022-09-19 14:02:20 -0700

    my files update

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - write string followed by  new line
+ * @str: string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while(*str)
+		-putchar(*str++);
+	_putchar('\n');
+}
+
