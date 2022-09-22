6a6d2fe9d5033e2c4de67b3cc11ab46ac7b3adbc
DavidJosh1 <stanleyjssh@gmail.com>
2022-09-22 16:34:36 -0700

    My main.h to repo

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
-
 #include "main.h"
-#include <stdio.h>
 /**
- * reset_to_98 - ur comment
- * @n:poiter
- * Return:void
+ * reset_to_98 - updates the value of points n to 98
+ *
+ * @n: input
+ * Return: void
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
-
 }
+	
